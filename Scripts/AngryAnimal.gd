23855a384526e6723333de647560989f96c0f522
Kublite <127664985+Kublite@users.noreply.github.com>
2023-05-04 19:31:25 +0300

    Add files via upload
    мобы плохо двигались, а после добавления функции преследования, вообще стояли afk пока персонаж к ним не подойдет. Ошибка была в функции search_for_target, из-за которой nps не понимали, что делать когда игрок вне радиуса

--- Scripts/AngryAnimal.gd
+++ Scripts/AngryAnimal.gd
@@ -3,47 +3,56 @@ extends "res://Scripts/Skeletman.gd"
 var stands = true
 var destination = Vector2()
 var velocity = Vector2()
-
-
+var prev_pos = Vector2()
+var target = null
+var default_speed = 70
 
 func _ready():
-	speed = 145
+	speed = default_speed
+	$StandingTimer.start(2)
 
-# warning-ignore:unused_argument
 func _process(delta):
 	if velocity:
-# warning-ignore:return_value_discarded
+		prev_pos = position
 		move_and_slide(velocity)
 		position.x = clamp(position.x, 0 ,10000)
 		position.y = clamp(position.y, 0 ,10000)
 	wander()
+	search_for_target()
+
+func search_for_target():
+	var pl =  get_parent().get_parent().get_player()
+	if position.distance_to(pl.position) < 200:
+		cancel_movement()
+		speed = default_speed * 3
+		target = pl
+		set_destination(target.position)
+	else:
+		if stands == true:
+			speed = default_speed
+			set_destination(Vector2())
 
 func set_destination(dest):
 	destination = dest
-	velocity = (destination - position).normalized()*speed
+	velocity = (destination - position).normalized() * speed
 	stands = false
 
 func cancel_movement():
 	velocity = Vector2()
-	destination = Vector2()
+	stands = true
 	$StandingTimer.start(2)
 
 func wander():
 	var pos = position
 	if stands:
 		randomize()
-		var x = int(rand_range(pos.x - 150, pos.x + 150))
-		var y = int(rand_range(pos.x - 150, pos.x + 150))
-		
+		var x = int(rand_range(pos.x - 300, pos.x + 300))
+		var y = int(rand_range(pos.y - 300, pos.y + 300))
 		x = clamp(x, 0, 10000)
-		y = clamp(x, 0, 10000)
-		
+		y = clamp(y, 0, 10000)
 		set_destination(Vector2(x,y))
-		
-	elif velocity != Vector2():
-		if pos.distance_to(destination) <= speed:
-			cancel_movement()
+	elif velocity != Vector2() and pos.distance_to(destination) <= speed:
+		cancel_movement()
 
 func _on_StandingTimer_timeout():
 	stands = true
-	pass # Replace with function body.
